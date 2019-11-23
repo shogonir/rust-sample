fc1944a0101caa4f3ea4c06543a7285d09f01a25
Shogo N <shogonir@gmail.com>
2019-11-24 01:00:41 +0900

    [modify] p03 complete

--- p03-guessing-game/src/main.rs
+++ p03-guessing-game/src/main.rs
@@ -1,14 +1,41 @@
+extern crate rand;
+
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
+
+    // at first
     println!("Guess the number!");
+    let mut count = 0;
 
-    println!("Please input your guess.");
+    // generate secret number
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    let mut guess = String::new();
+    loop {
+        // receive input number
+        println!("Please input your guess.");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
-    
-    println!("You guessed: {}", guess);
+        // convert string to number
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        
+        println!("You guessed: {}", guess);
+        count += 1;
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less      => println!("Too small!"),
+            Ordering::Greater   => println!("Too big!"),
+            Ordering::Equal     => {
+                println!("You won with {} answers.", count);
+                break;
+            }
+        }
+    }
 }
